b68d3d361290621cb698ac7b129a6a1beea37991
Stefan Probst <stefanprobst@posteo.de>
2022-06-17 14:29:23 +0200

    fix: add d.ts

---
+++ src/index.d.ts
@@ -0,0 +1,10 @@
+import type { Plugin } from 'unified'
+import type * as Mdast from 'mdast'
+
+export interface Options {
+  assetPrefix?: string
+}
+
+declare const withNextImages: Plugin<[Options?], Mdast.Root>
+
+export default withNextImages
